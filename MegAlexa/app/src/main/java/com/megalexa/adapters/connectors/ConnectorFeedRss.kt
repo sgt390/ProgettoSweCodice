6db29aed8567153c1357f78e9a0cab5650219d00
ludobrocca <lbrocca29@gmail.com>
2019-02-16 21:23:14 +0100

    prove test

--- MegAlexa/app/src/main/java/com/megalexa/adapters/connectors/ConnectorFeedRss.kt
+++ MegAlexa/app/src/main/java/com/megalexa/adapters/connectors/ConnectorFeedRss.kt
@@ -1,12 +1,33 @@
 package com.megalexa.adapters.connectors
+import android.webkit.URLUtil
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.IOException
+import java.io.InputStream
+import java.net.MalformedURLException
+import java.net.URL
+import java.net.URLConnection
 
-class ConnectorFeedRss(private val uri: String):Connector {
+
+class ConnectorFeedRss(private var url: String):Connector {
     init {
-        val feedMessage:String = connect(uri)
+        url=connect(url)
     }
 
-    override fun connect(uri: String):String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun connect(url: String):String {
+
+    if (valid()) {
+
+        return url
+    }else{
+
+        return "ERROR_INVALID_URL"
+
+    }
+
+
+
     }
 
     /**
@@ -14,7 +35,70 @@ class ConnectorFeedRss(private val uri: String):Connector {
      * @return feed is valid
      */
     override fun valid():Boolean {
-        TODO(reason = "not implemented")
+
+        return URLUtil.isHttpsUrl(url) && isRssFeed()
+    }
+
+    /**
+     * isRssFeed checks if the content received from the URL returns a correct rss feed in xml format
+     * @returns true if the url is an rssFeed
+     */
+    private fun isRssFeed(): Boolean {
+
+        val resource:URL
+        val xpp: XmlPullParser
+        val iStream:InputStream
+        var result = false
+
+        try {
+
+            resource = URL(url)
+
+            val factory = XmlPullParserFactory.newInstance()
+
+            xpp = factory.newPullParser()
+
+            iStream = getInputStream(resource)
+
+            iStream.use {
+                    x -> xpp.setInput(x, "UTF_8")
+
+
+                if(xpp.getName().equals("rss"))
+                    result =true
+
+            }
+
+
+        }catch (err: MalformedURLException) {
+
+            err.printStackTrace()
+
+        }catch (err: XmlPullParserException) {
+
+            err.printStackTrace()
+        }
+
+
+    return result
+    }
+
+
+
+
+    private fun getInputStream(resource: URL) : InputStream {
+
+        try {
+
+            return resource.openConnection().getInputStream()
+
+        }catch (err: IOException){
+
+            err.printStackTrace()
+
+        }
+
+        return resource.openConnection().getInputStream()
     }
 
 }
