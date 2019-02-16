4d868425997594f1d3056ee39aaaddd83d2ca42f
stefano <stefano.sgt39@gmail.com>
2019-02-16 22:34:12 +0100

    created and linked activities

--- MegAlexa/app/src/main/java/com/megalexa/activities/GeneralLoggedActivity.kt
+++ MegAlexa/app/src/main/java/com/megalexa/activities/GeneralLoggedActivity.kt
@@ -1,18 +1,70 @@
 package com.megalexa.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 import com.megalexa.R
+import kotlinx.android.synthetic.main.activity_workflow.*
+import android.content.Intent
+import android.R.id
 
 
-class GeneralLoggedActivity : AppCompatActivity() {
 
+
+
+class GeneralLoggedActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private lateinit var drawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_general_logged)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        drawer = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this, this.drawer, toolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        add_workflow.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v: View?) {
+        when(v){
+            add_workflow -> startActivity(Intent(this, CreateWorkflowActivity::class.java))
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            //R.id.menu_quit ->
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        }else {
+            super.onBackPressed()
+        }
     }
 
 
