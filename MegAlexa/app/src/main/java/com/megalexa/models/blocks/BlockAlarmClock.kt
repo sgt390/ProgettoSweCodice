bfce6c194069cee0ad798e9c630ab744f8a2647c
Andrea <andrea.deidda@studenit.unipd.it>
2019-02-18 16:55:13 +0100

    Update(piccolo) blocco sveglia

--- MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockAlarmClock.kt
+++ MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockAlarmClock.kt
@@ -1,15 +1,20 @@
 package com.megalexa.models.blocks
+import java.util.Calendar
+class BlockAlarmClock(private val minutes: Int, private val hours: Int,
+                      private val day: Int, private val mounth: Int, private val year: Int, month: Int) :Calendar{
 
-import java.text.DateFormat
-
-class BlockAlarmClock(private val time: DateFormat) : Block{
+    private var time = Calendar("$hours:$minutes",)
     init{
-        require(time. > 0){...}
+        require(minutes >= 0 && minutes < 60){println("")}
+        require(hours >= 0 && hours < 60){println("")}
+        require(day > 0){println("")}
+        require(mounth > 0){println("")}
     }
     //ritorna l'orario in cui suona la sveglia
-    override fun getInformation() : Block
-
-    fun alarmTime(): DateFormat{
+    fun getInformation() : String{
+        return "$year/$mounth/$day $hours:$minutes"
+    }
+    fun alarmTime(): {
         return time
     }
 
