ea658d0697eb56e20f76af1d1ee4cf26ebfe02d9
Andrea <andrea.deidda@studenit.unipd.it>
2019-02-20 19:52:31 +0100

    Update TextBlock

--- MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockTextBox.kt
+++ MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockTextBox.kt
@@ -1,10 +1,9 @@
 package com.megalexa.models.blocks
 
 class BlockTextBox(private val textBox: String):Block {
-    override fun getInformation() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getInformation(): String {
+        return textBox //To change body of created functions use File | Settings | File Templates.
     }
-    fun read() = textBox
 }
 
 //prove
