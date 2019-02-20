6872bce6b722ce0831f98c8fb0b6de5893480259
Ludovico Brocca <lbrocca29@gmail.com>
2019-02-20 17:25:19 +0100

    design fragment

--- MegAlexa/app/src/main/java/com/megalexa/activities/CreateBlockActivity.kt
+++ MegAlexa/app/src/main/java/com/megalexa/activities/CreateBlockActivity.kt
@@ -4,11 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
 import com.megalexa.R
 import kotlinx.android.synthetic.main.activity_create_block.*
-import kotlinx.android.synthetic.main.activity_create_workflow.*
+import kotlinx.android.synthetic.main.recycler_view_item.*
+import com.megalexa.fragments.RssFragment
 
 class CreateBlockActivity: AppCompatActivity(), View.OnClickListener {
 
@@ -20,6 +19,24 @@ class CreateBlockActivity: AppCompatActivity(), View.OnClickListener {
 
 
 
+
+        rssBlock.setOnClickListener {
+            // Get the text fragment instance
+            val textFragment = RssFragment()
+
+            // Get the support fragment manager instance
+            val manager = supportFragmentManager
+
+            // Begin the fragment transition using support fragment manager
+            val transaction = manager.beginTransaction()
+
+            // Replace the fragment on container
+            transaction.replace(R.id.fragment_container,textFragment)
+            transaction.addToBackStack(null)
+
+            // Finishing the transition
+            transaction.commit()
+        }
 
 
     }
--- MegAlexa/app/src/main/java/com/megalexa/fragments/RssFragment.kt
+++ MegAlexa/app/src/main/java/com/megalexa/fragments/RssFragment.kt
@@ -1,20 +1,66 @@
 package com.megalexa.fragments
 
-/*import android.os.Bundle
+import android.content.Context
+import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import com.megalexa.R
 
 class RssFragment : Fragment(){
 
-    companion object {
-        fun newInstance(): RssFragment {
-            return RssFragment()
-        }
+  /*  override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
     }
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View?{
-        return inflater.inflate(R.layout.fragment_user_menu, container, false)
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Get the custom view for this fragment layout
+        //val view = inflater!!.inflate(R.layout.rss_fragment_layout,container,false)
+
+        // Get the text view widget reference from custom layout
+        //val tv = view.findViewById<TextView>(R.id.text_view)
+
+        // Set a click listener for text view object
+        /*tv.setOnClickListener{
+            // Change the text color
+            tv.setTextColor(Color.RED)
+
+            // Show click confirmation
+            Toast.makeText(view.context,"TextView clicked.", Toast.LENGTH_SHORT).show()
+        }
+
+        // Return the fragment view/layout
+        */return view
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }*/
+
+}
