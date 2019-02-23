b05b483da12927c8bbac107b4ebaca28bd6a4893
ludobrocca <lbrocca29@gmail.com>
2019-02-23 16:37:05 +0100

    Struttura visualizzazione workflow

---
+++ MegAlexa/app/src/main/java/com/megalexa/adapters/view/WorkflowViewAdapter.kt
@@ -0,0 +1,32 @@
+package com.megalexa.adapters.view
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import com.megalexa.models.workflow.Workflow
+
+class WorkflowViewAdapter(private val dataset: ArrayList<Workflow>):RecyclerView.Adapter<WorkflowViewHolder>(){
+
+
+    override fun onBindViewHolder(p0: WorkflowViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): WorkflowViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+
+
+
+class WorkflowViewHolder(v: View): RecyclerView.ViewHolder(v) {
+
+
+}
