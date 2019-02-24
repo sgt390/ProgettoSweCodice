adf6a2adc92014b3a81c677959edc33df41ee8de
Andrea <andrea.deidda@studenit.unipd.it>
2019-02-24 18:07:35 +0100

    Update sveglia

--- MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockAlarmClock.kt
+++ MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockAlarmClock.kt
@@ -11,24 +11,25 @@
  *
  */
 package com.megalexa.models.blocks
-import android.content.Context
 import java.util.Calendar
 import android.media.Ringtone
-import android.media.RingtoneManager
+
 class BlockAlarmClock(private var minutes: Int, private var hours: Int,
                       private var sound: Ringtone) : Block{
     val calendar = Calendar.getInstance()
     //set calendar
     init {
+        //controls will do in fragment file later
         calendar.set(Calendar.HOUR_OF_DAY, hours)
         calendar.set(Calendar.MINUTE, minutes)
         calendar.set(Calendar.SECOND, 0)
     }
 
     //Set method
-    fun setAlarm(hour: Int,minute: Int){
+    fun setAlarm(hour: Int,minute: Int, noise: Ringtone){
         minutes = minute
         hours = hour
+        sound = noise
         calendar.set(Calendar.HOUR_OF_DAY, hour)
         calendar.set(Calendar.MINUTE, minute)
     }
@@ -36,7 +37,7 @@ class BlockAlarmClock(private var minutes: Int, private var hours: Int,
     override fun getInformation() : String{
         return "Alarm Clock block information:" +
                 "\tTime ${calendar.get(Calendar.HOUR_OF_DAY)}:${calendar.get(Calendar.MINUTE)}"
-                //+ "\tRingtone ${sound.getTitle(getText())}"
+                //+ "\tRingtone ${sound.getTitle()}"
     }
     //Return alarm clock set time
     fun alarmTime(): String {
