b64d5534a229b3adef53fd3feccf90872024a9cd
Ludovico Brocca <lbrocca29@gmail.com>
2019-03-02 16:38:25 +0100

    aggiunti permessi sul manifest

--- MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockWeather.kt
+++ MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockWeather.kt
@@ -1,4 +1,24 @@
 package com.megalexa.models.blocks
 
-class BlockWeather {
+import android.location.Location
+
+
+class BlockWeather(private val location: Location) :Block{
+
+    private var longitude= location.longitude
+    private var latitude=location.latitude
+
+
+    override fun getInformation(): String {
+        return "longitude:  $longitude latitude : $latitude"
+    }
+
+
+    fun getLongitude() :Double {
+        return longitude
+    }
+
+    fun getLatitude(): Double {
+        return latitude
+    }
 }
