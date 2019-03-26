ad879aa0026adacbbfe8b016404c673881dadae6
Ludovico Brocca <lbrocca29@gmail.com>
2019-03-26 15:50:52 +0100

    aggiunto metodo di utilità + codificato test

---
+++ MegAlexa/app/src/androidTest/java/com/megalexa/BlockFilterTest.kt
@@ -0,0 +1,47 @@
+package com.megalexa
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.megalexa.models.blocks.BlockFeedRss
+import com.megalexa.models.blocks.BlockTextToSpeech
+import com.megalexa.models.blocks.Filter
+import com.megalexa.models.connectors.ConnectorAmazonMusic
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class BlockFilterTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.megalexa", appContext.packageName)
+    }
+
+    @Test
+    fun withFilterableBlock() {
+        val filter = Filter(5)
+
+        val block= BlockFeedRss("https://feedforall.com/sample.xml")
+        filter.attachTo(block)
+        assertEquals(true,filter.getResult())
+    }
+    
+    @Test
+    fun withNonFilterableBlock() {
+        val filter = Filter(5)
+        val block= BlockTextToSpeech("Prova")
+        filter.attachTo(block)
+        assertEquals(false,filter.getResult())
+
+    }
+
+}
--- MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockFilter.kt
+++ MegAlexa/app/src/main/java/com/megalexa/models/blocks/BlockFilter.kt
@@ -7,6 +7,7 @@ import org.json.JSONObject
  */
 internal class Filter(private val limit:Short):Block {
     private var filteringResult= false
+    private lateinit var attachedBlock: Block
 
     override fun getInformation() :String {
 
@@ -23,17 +24,21 @@ internal class Filter(private val limit:Short):Block {
      * checks if block is filtrable, upper classes will add filters based on the result
      */
     fun attachTo(block: Block) {
-        if(block is Filtrable)
+        if(block is Filtrable) {
+            attachedBlock = block
             notifyResult(true)
-        else
+        }else {
             notifyResult(false)
+        }
     }
 
     private fun notifyResult(bool:Boolean) {
         filteringResult=bool
     }
 
-    fun getResult(): Boolean {
-        return filteringResult
-    }
+    fun getResult() = filteringResult
+
+
+    fun attachedBlock() = attachedBlock.getInformation()
+
 }
