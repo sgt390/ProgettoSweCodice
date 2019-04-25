ef7c85e2013800d4576885a29d5c86d0e417ef19
Bratzu93 <ziobratzu@gmail.com>
2019-04-25 16:58:48 +0200

    weath to do

--- MegAlexa/app/src/main/java/com/megalexa/ui/fragments/WeatherFragment.kt
+++ MegAlexa/app/src/main/java/com/megalexa/ui/fragments/WeatherFragment.kt
@@ -15,8 +15,13 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.JsonObject
 import com.megalexa.R
-import com.megalexa.ui.activities.CreateBlockActivity
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.URL
+import java.net.URLEncoder
+import javax.net.ssl.HttpsURLConnection
 
 class WeatherFragment: Fragment(){
 
@@ -35,39 +40,66 @@ class WeatherFragment: Fragment(){
             }
             else{
                 city= editText.text.toString()
-find_weather()
 //                val activity= activity as CreateBlockActivity
 //                activity.onFragmentClick(this)
+                val prova = FromJSON(getOperation())
+                Toast.makeText(context,prova , Toast.LENGTH_SHORT).show()
             }
 
         }
 
-find_weather()
+
         return view
     }
 
-    private fun find_weather() {
-
-        val queue = Volley.newRequestQueue(context)
-        val url = "https://api.openweathermap.org/data/2.5/weather?q=Padova&APPID=4b1ea0b33edc40ba538b366b98484801"
-
-// Request a string response from the provided URL.
-        val stringRequest = StringRequest(Request.Method.GET, url,
-            Response.Listener<String> { response ->
-                // Display the first 500 characters of the response string.
-                 Toast.makeText(context,"Response is: ${response.substring(0, 500)}",Toast.LENGTH_LONG).show()
-            },
-            Response.ErrorListener { Toast.makeText(context,"error",Toast.LENGTH_LONG).show() })
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest)
-
-//        val api ="api.openweathermap.org/data/2.5/weather?q="
-//        var url: URL = api + city + "&APPID=4b1ea0b33edc40ba538b366b98484801"
-//
-//     url.openConnection().getInputStream()
-
+     fun FromJSON(jsonObject: JSONObject): String? {
+        return jsonObject.getJSONObject("coord").getString("lon")
     }
+    fun getOperation(): JSONObject {
+
+
+        val api ="api.openweathermap.org/data/2.5/weather?q="
+        var url = api + city + "&APPID=4b1ea0b33edc40ba538b366b98484801"
+
+
+
+
+            var json= JSONArray()
+
+            //  val url= "api.openweathermap.org/data/2.5/weather?q="
+            val myURL = URL("$url")
+            with(myURL.openConnection() as HttpsURLConnection) {
+    //            setRequestProperty("Content-Type", "application/json")
+    //            requestMethod= "GET"
+    //            println("URL : $url")
+    //            println("Response Code : $responseCode")
+    //            BufferedReader(InputStreamReader(inputStream)).use {
+    //                val response = StringBuffer()
+    //                var inputLine = it.readLine()
+    //                while (inputLine != null) {
+    //                    response.append(inputLine)
+    //                    inputLine = it.readLine()
+    //                }
+    //                json= JSONArray(response.toString())
+    //            }
+            }
+            return JSONObject().put("content",json)
+        }
+//        val queue = Volley.newRequestQueue(context)
+//        val url = "https://api.openweathermap.org/data/2.5/weather?q=Padova&APPID=4b1ea0b33edc40ba538b366b98484801"
+//
+//// Request a string response from the provided URL.
+//        val stringRequest = StringRequest(Request.Method.GET, url,
+//            Response.Listener<String> { response ->
+//                // Display the first 500 characters of the response string.
+//                 Toast.makeText(context,"Response is: ${response.substring(0, 500)}",Toast.LENGTH_LONG).show()
+//            },
+//            Response.ErrorListener { Toast.makeText(context,"error",Toast.LENGTH_LONG).show() })
+//
+//// Add the request to the RequestQueue.
+//        queue.add(stringRequest)
+//
+//    }
 
     fun getText(): String{
         return city
--- MegAlexa/app/src/main/java/com/megalexa/util/service/BlockWeatherService.kt
+++ MegAlexa/app/src/main/java/com/megalexa/util/service/BlockWeatherService.kt
@@ -1,15 +1,49 @@
 package com.megalexa.util.service
 
-import com.megalexa.models.blocks.BlockSport
+
+import com.megalexa.models.blocks.BlockWeather
+import org.json.JSONArray
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.net.URLEncoder
+import javax.net.ssl.HttpsURLConnection
 
 object BlockWeatherService :BlockService() {
 
-    override fun convertFromJSON(jsonObject: JSONObject): BlockSport {
+    override fun convertFromJSON(jsonObject: JSONObject): BlockWeather {
         TODO()
     }
 
     override fun <T> convertToJSON(t: T): JSONObject {
         TODO()
+    }
+
+    override fun getOperation(params: List<Pair<String, String>>): JSONObject {
+        var json= JSONArray()
+        val query = StringBuilder()
+        for (item in params) {
+            query.append(URLEncoder.encode(item.first,"UTF-8")+"="+ URLEncoder.encode(item.second,"UTF-8")+ "&")
+        }
+        val string=query.substring(0,query.length-1)
+        val url= "api.openweathermap.org/data/2.5/weather?q="
+        val myURL = URL("$url?$string")
+        with(myURL.openConnection() as HttpsURLConnection) {
+            setRequestProperty("Content-Type", "application/json")
+            requestMethod= "GET"
+            println("URL : $url")
+            println("Response Code : $responseCode")
+            BufferedReader(InputStreamReader(inputStream)).use {
+                val response = StringBuffer()
+                var inputLine = it.readLine()
+                while (inputLine != null) {
+                    response.append(inputLine)
+                    inputLine = it.readLine()
+                }
+                json= JSONArray(response.toString())
+            }
+        }
+        return JSONObject().put("content",json)
     }
 }
