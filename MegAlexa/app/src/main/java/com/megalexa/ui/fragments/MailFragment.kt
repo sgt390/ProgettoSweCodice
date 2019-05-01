64d32c7888673deb547bee8c5476f25fa9c3b55f
Bratzu93 <ziobratzu@gmail.com>
2019-05-01 12:50:58 +0200

    fix fragment

--- MegAlexa/app/src/main/java/com/megalexa/ui/fragments/MailFragment.kt
+++ MegAlexa/app/src/main/java/com/megalexa/ui/fragments/MailFragment.kt
@@ -22,26 +22,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.NumberPicker
 import android.widget.Toast
 import com.megalexa.R
 import com.megalexa.ui.activities.CreateBlockActivity
 
 class MailFragment: Fragment() {
-    private var cardinality = 0
+    private var cardinality = 1
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.block_mail_layout, container, false)
         val confirm = view.findViewById<Button>(R.id.confirm)
         val filter = view.findViewById<Button>(R.id.button_filter)
-        val mail = view.findViewById<Button>(R.id.mail)
-        val password = view.findViewById<Button>(R.id.password)
+        val mail = view.findViewById<EditText>(R.id.mail_gmail)
+        val password = view.findViewById<EditText>(R.id.password_gmail)
 
         filter.setOnClickListener{
             val builder = AlertDialog.Builder(ContextThemeWrapper(context,R.style.Theme_AppCompat_Dialog))
             val pickerLayout =inflater.inflate(R.layout.simple_number_picker,null)
             val picker=pickerLayout.findViewById<NumberPicker>(R.id.number_picker)
-            picker.minValue=0
-            picker.maxValue=10
+            picker.minValue=1
+            picker.maxValue=5
 
             val confirmFilter = {
                     _: DialogInterface, _:Int -> cardinality = picker.value
