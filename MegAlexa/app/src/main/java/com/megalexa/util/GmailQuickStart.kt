2ae2282beee6b7c6cca14c652aa227310b56503f
Andrea <andrea.deidda@studenit.unipd.it>
2019-05-05 18:08:22 +0200

    update

--- MegAlexa/app/src/main/java/com/megalexa/util/GmailQuickStart.kt
+++ MegAlexa/app/src/main/java/com/megalexa/util/GmailQuickStart.kt
@@ -4,40 +4,27 @@ import com.google.api.client.auth.oauth2.Credential
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeRequestUrl
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.client.util.store.FileDataStoreFactory
-import com.google.api.services.gmail.Gmail
-import com.google.api.services.gmail.GmailRequest
 import com.google.api.services.gmail.GmailScopes
-import com.google.api.services.gmail.model.Label
-import com.google.api.services.gmail.model.ListLabelsResponse
 import java.io.*
 
-import java.security.GeneralSecurityException
 import java.util.Collections
 
 class GmailQuickStart {
-    private lateinit var HTTP_TRANSPORT: NetHttpTransport
-    init {
-        HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
-        var service = Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, this.getCredentials(HTTP_TRANSPORT))
-            .setApplicationName(APPLICATION_NAME)
-            .build();
-    }
-    private val APPLICATION_NAME: String = "Gmail API Java Quickstart"
+
     private val JSON_FACTORY: JacksonFactory = JacksonFactory.getDefaultInstance()
     private val TOKENS_DIRECTORY_PATH: String = "tokens"
 
     private val SCOPES: List<String> = Collections.singletonList(GmailScopes.GMAIL_LABELS)
     private val CREDENTIALS_FILE_PATH: String = "/credentials.json"
 
-    private fun getCredential(HTTP_TRANSPORT: NetHttpTransport): Credential{
-        val input: InputStream = GmailQuickStart.
+    @Throws(IOException::class)
+    fun getCredential(HTTP_TRANSPORT: NetHttpTransport): Credential{
+        val input = this::class.java.getResourceAsStream(CREDENTIALS_FILE_PATH)
+            ?: throw FileNotFoundException("Resource not found: $CREDENTIALS_FILE_PATH")
         val clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(JSON_FACTORY, InputStreamReader(input))
 
         val flow: GoogleAuthorizationCodeFlow = GoogleAuthorizationCodeFlow.Builder(
--- MegAlexa/app/src/main/java/com/megalexa/util/service/BlockReadEmailService.kt
+++ MegAlexa/app/src/main/java/com/megalexa/util/service/BlockReadEmailService.kt
@@ -1,22 +1,13 @@
 package com.megalexa.util.service
 
-//import com.google.api.client.auth.oauth2.Credential
-//import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-//import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-//import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-//import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
-//import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-//import com.google.api.client.http.javanet.NetHttpTransport
-//import com.google.api.client.json.jackson2.JacksonFactory
-//import com.google.api.client.util.DateTime
-//import com.google.api.client.util.store.FileDataStoreFactory
-//import com.google.api.services.calendar.Calendar
-//import com.google.api.services.calendar.CalendarScopes
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.gmail.Gmail
+import com.megalexa.util.GmailQuickStart
 import org.json.JSONObject
-import java.io.File
-import java.io.FileNotFoundException
 import java.io.IOException
-import java.io.InputStreamReader
 import java.security.GeneralSecurityException
 
 object BlockReadEmailService:BlockService() {
@@ -25,9 +16,31 @@ object BlockReadEmailService:BlockService() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    @Throws(IOException::class, GeneralSecurityException::class)
     override fun <BlockReadEmail> convertToJSON(t: BlockReadEmail): JSONObject {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val blockMail = t as com.megalexa.models.blocks.BlockReadEmail
+        val allBlock = JSONObject()
+        allBlock.put("blockType", "Mail")
+        val config = JSONObject()
+        config.put("URL", "blockMail")
+        allBlock.put("config", config)
+        return allBlock
     }
 
+    fun getToken(): Credential {
+        val HTTP_TRANSPORT: NetHttpTransport = GoogleNetHttpTransport.newTrustedTransport()
+        val JSON_FACTORY: JacksonFactory = JacksonFactory.getDefaultInstance()
+        val token = GmailQuickStart().getCredential(HTTP_TRANSPORT)
+        var service = Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, token)
+            .setApplicationName("Megalexa")
+            .build()
+
+        println("access_token= " + token.accessToken)
+        println("expires in = " + token.expiresInSeconds!!)
+        println("refresh_token= " + token.refreshToken)
+        println("serverEncoded= " + token.tokenServerEncodedUrl)
+        return token
+    }
 
 }
