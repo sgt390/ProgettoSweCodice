d2ae187a8eb3fc44b5dcac0d99b165f35aaf21ca
Andrea <andrea.deidda@studenit.unipd.it>
2019-05-05 14:36:02 +0200

    Update layout e mailfragment

--- MegAlexa/app/src/main/java/com/megalexa/ui/fragments/CalendarFragment.kt
+++ MegAlexa/app/src/main/java/com/megalexa/ui/fragments/CalendarFragment.kt
@@ -2,8 +2,8 @@ package com.megalexa.ui.fragments
 
 import android.app.AlertDialog
 import android.content.DialogInterface
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
@@ -12,11 +12,11 @@ import android.widget.Button
 import android.widget.NumberPicker
 import com.megalexa.R
 import com.megalexa.ui.activities.CreateBlockActivity
-import com.megalexa.util.service.BlockReadEmailService
+import com.megalexa.util.ApplicationContextProvider.Companion.context
+import com.megalexa.util.service.BlockCalendarService
 import org.jetbrains.anko.doAsync
 
-class MailFragment: Fragment(){
-
+class CalendarFragment: Fragment() {
     private var cardinality = 5
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -26,7 +26,7 @@ class MailFragment: Fragment(){
         val button= view.findViewById<Button>(R.id.confirmItemBotton_mail)
 
         filter.setOnClickListener{
-            val builder = AlertDialog.Builder(ContextThemeWrapper(context,R.style.Theme_AppCompat_Dialog))
+            val builder = AlertDialog.Builder(ContextThemeWrapper(context, R.style.Theme_AppCompat_Dialog))
             val pickerLayout =inflater.inflate(R.layout.simple_number_picker,null)
             val picker=pickerLayout.findViewById<NumberPicker>(R.id.number_picker)
             picker.minValue=1
@@ -48,14 +48,13 @@ class MailFragment: Fragment(){
             builder.show()
         }
         button.setOnClickListener {
-            //doAsync {  BlockReadEmailService.getCredentials(BlockReadEmailService.HTTP_TRANSPORT)}
-                val activity= activity as CreateBlockActivity
-                activity.onFragmentClick(this)
+            doAsync {  BlockCalendarService.getCredentials(BlockCalendarService.HTTP_TRANSPORT)}
+            val activity= activity as CreateBlockActivity
+            activity.onFragmentClick(this)
         }
 
         return view
     }
 
     fun getCardinality() = cardinality
-
 }
