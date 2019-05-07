f7ddaaea013b9e2f19359407632dd15c279f348b
mirkofranco <mirko.franco@icloud.com>
2019-05-07 16:25:06 +0200

    Google key

--- MegAlexa/app/src/main/java/com/megalexa/ui/activities/GoogleActivity.kt
+++ MegAlexa/app/src/main/java/com/megalexa/ui/activities/GoogleActivity.kt
@@ -4,34 +4,24 @@ package com.megalexa.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.megalexa.R
 import kotlinx.android.synthetic.main.mail_fragment_layout.*
-import android.support.annotation.NonNull
-import android.util.Log
+
 import com.google.android.gms.tasks.OnCompleteListener
-import com.amazon.identity.auth.device.api.authorization.AuthorizationManager.signOut
-import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.store.FileDataStoreFactory
+
+
 import com.google.api.services.calendar.CalendarScopes
 import com.megalexa.util.ApplicationContextProvider
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.doAsyncResult
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.InputStreamReader
 
 
 const val RC_SIGN_IN = 123
@@ -43,18 +33,19 @@ class GoogleActivity : AppCompatActivity() {
         private val SCOPES = listOf(CalendarScopes.CALENDAR)
         private val TOKENS_DIRECTORY_PATH = "tokens"
     }
+    lateinit var googleSignInClient : GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mail_fragment_layout)
 
         // Configure sign-in to request the user's ID, email address, and basic
 // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(ApplicationContextProvider.context!!.resources!!.getString(R.string.client_Id))
-            .requestEmail()
-            .build()
+        //val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+          //  .requestIdToken(ApplicationContextProvider.context!!.resources!!.getString(R.string.client_Id))
+            //.requestEmail()
+            //.build()
         // Build a GoogleSignInClient with the options specified by gso.
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        //val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
         ///////////////////////////////////////////////////////////////////////
         //      MY CODE FROM QUICKSTART
 
@@ -72,6 +63,8 @@ class GoogleActivity : AppCompatActivity() {
 //        println("client: " + flow.clientAuthentication)
 //        val receiver: LocalServerReceiver = LocalServerReceiver.Builder().setPort(8888).build()
 //        val Tok  = doAsyncResult { AuthorizationCodeInstalledApp(flow,receiver).authorize("user") }
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(ApplicationContextProvider.context!!.resources!!.getString(R.string.google_client_id)).requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this, options)
 
 
         ///////////////////////////////////////////////////////////////////////////////////////
@@ -79,7 +72,7 @@ class GoogleActivity : AppCompatActivity() {
         tv_name.visibility = View.GONE
         sign_in_button.setSize(SignInButton.SIZE_STANDARD)
         sign_in_button.setOnClickListener{
-            val signInIntent = mGoogleSignInClient.signInIntent
+            val signInIntent = googleSignInClient.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
         }
         val acct = GoogleSignIn.getLastSignedInAccount(this)
@@ -89,10 +82,10 @@ class GoogleActivity : AppCompatActivity() {
             tv_name.visibility = View.VISIBLE
 //            Log.d("Tag",acct.idToken)
         }
-        mGoogleSignInClient.signOut()
+        /*mGoogleSignInClient.signOut()
             .addOnCompleteListener(this, OnCompleteListener<Void> {
                 // ...
-            })
+            })*/
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -111,7 +104,8 @@ class GoogleActivity : AppCompatActivity() {
             val account = completedTask.getResult(ApiException::class.java)
             sign_in_button.visibility = View.GONE
             if (account != null) {
-//                Log.d("tag",account.idToken)
+                val ciao = account.idToken
+                Log.d("tag", account.idToken)
                 tv_name.text = account.displayName
             }
             tv_name.visibility = View.VISIBLE
