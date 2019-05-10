9d2687877e51f732ce5e18340d8c0051bba6ce23
Ludovico Brocca <lbrocca29@gmail.com>
2019-05-10 15:44:18 +0200

    aggiunti tests

---
+++ MegAlexa/app/src/androidTest/java/com/megalexa/block/BlockCryptoTest.kt
@@ -0,0 +1,16 @@
+package com.megalexa.block
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BlockCryptoTest:BlockTest {
+
+    override fun invalidBLock() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun validBlock() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ MegAlexa/app/src/androidTest/java/com/megalexa/block/BlockTwitterTest.kt
@@ -0,0 +1,19 @@
+package com.megalexa.block
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BlockTwitterTest: BlockTest {
+
+    @Test
+    override fun validBlock() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    @Test
+    override fun invalidBLock() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
